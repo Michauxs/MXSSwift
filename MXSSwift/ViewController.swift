617004ef3f8ed54dd1b02ffdce8f4e2d4cff53fa
Michauxs <363335983@qq.com>
2017-10-24 11:26:35 +0800

    Initial Commit

---
+++ MXSSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MXSSwift
+//
+//  Created by Alfred Yang on 24/10/17.
+//  Copyright © 2017年 MXS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
